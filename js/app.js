28daa054a468872762b49cbe45452ce100f4ebbe
Florent Lecocq <lecocqflorent@outlook.fr>
2023-06-05 11:14:05 +0200

    avancée vanilla js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-window.alert("coucou");
+
+localStorage.tasks = JSON.stringify([
+    {id: 1, content: "Tâche 1", completed: true},
+    {id: 2, content: "Tâche 2", completed: false}
+]);
+
+
+
+if (localStorage.tasks === undefined) {
+  localStorage.tasks = JSON.stringify([]);
+}
+
+// console.table(JSON.parse(localStorage.tasks));
+const tasks = localStorage.tasks;
+
+tasks.forEach(task => {
+  const add = document.createElement("li");
+  if (task.completed) {
+    li.classList.add('completed');
+  } 
+  add.innerHTML = '<div class="view"><input class="toggle" type="checkbox" checked /><label>' + task.content + '</label><button class="destroy"></button></div>';
+  document.querySelector(".todo-list").appendChild(add);
+});
+
+console.log('script chargé');
+// document.querySelectorAll('.todo-input').forEach((trigger)=> {
+//     trigger.addEventListener("keyup", function(e) {
+//         if(e.key === "Enter"){
+//             const add = document.createElement("li");
+//             add.innerHTML = this.value;
+//             document.querySelector(".todos-lists").appendChild(add);
+//             document.querySelector(".todo-input").value = "";
+//         }})
+// })
+
+
